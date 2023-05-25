71fc4350c90ed9abba986c748773548c60a7ff89
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-05-25 20:31:33 -0300

    feat: deleted dist folder that generated application files in javascript

--- dist/middlewares/ensureDataIsValid.middeware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ensureDataIsValidMiddleware = void 0;
-const ensureDataIsValidMiddleware = (schema) => (request, _, next) => {
-    const validatedData = schema.parse(request.body);
-    request.body = validatedData;
-    return next();
-};
-exports.ensureDataIsValidMiddleware = ensureDataIsValidMiddleware;
